d819a924b75c1eb33697245aa098e5070424ba13
SydneyHerndon <sydneyherndon19@gmail.com>
2020-03-29 11:50:32 -0400

    Combined together each piece to finalzie question

--- sql_challenge/sql/question_1-data_cleaning.sql
+++ sql_challenge/sql/question_1-data_cleaning.sql
@@ -17,3 +17,21 @@ from continent_map
 group by country_code
 having count(*) > 1
 order by country_code;
+
+-- Getting N/A to display first 
+select COALESCE(country_code, 'N/A') as country_code, count(*)
+from continent_map
+group by country_code
+having count(*) > 1
+ORDER BY case when COALESCE(country_code, 'N/A')  = 'N/A' then 1 else 2 end, country_code; 
+
+-- It is unclear whether the output needs the count of country codes. 
+-- Here is an option if you only want the names. 
+select COALESCE(country_code, 'N/A') as country_code
+from continent_map
+group by country_code
+having count(*) > 1
+ORDER BY case when COALESCE(country_code, 'N/A')  = 'N/A' then 1 
+         else 2
+         end, 
+         country_code; 
